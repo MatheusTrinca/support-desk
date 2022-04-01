ba0174ae98071c51bf3ecbe9b69d5915b1a4c3cf
Matheus Trinca <matheusfelipetrinca@gmail.com>
2022-04-01 16:11:36 -0300

    users initial

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,14 @@
-console.log('Server running...');
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome to Support Desk API' });
+});
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes'));
+
+app.listen(PORT, () => console.log(`Server Running on PORT ${PORT}`));

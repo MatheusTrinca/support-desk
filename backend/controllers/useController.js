fc87893effcdbe217750a89e8b82961b20132625
Matheus Trinca <matheusfelipetrinca@gmail.com>
2022-04-04 14:53:25 -0300

    jwt sign

--- backend/controllers/useController.js
+++ backend/controllers/useController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require('express-async-handler');
 const bcrypt = require('bcryptjs');
 const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
 
 // @desc   Register a new user
 // @route  POST /api/users
@@ -28,7 +29,7 @@ const registerUser = asyncHandler(async (req, res) => {
   const user = await User.create({
     name,
     email,
-    hashedPassword,
+    password: hashedPassword,
   });
 
   if (user) {
@@ -36,6 +37,7 @@ const registerUser = asyncHandler(async (req, res) => {
       _id: user._id,
       name: user.name,
       email: user.email,
+      token: generateToken(user._id),
     });
   } else {
     res.status(400);
@@ -47,10 +49,28 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route  POST /api/users/login
 // @access Public
 const loginUser = asyncHandler(async (req, res) => {
-  res.send('Login User');
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error('Email ou Senha inválidos');
+  }
 });
 
-module.exports = {
-  registerUser,
-  loginUser,
+// Generate token
+const generateToken = id => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  });
 };
+
+module.exports = { registerUser, loginUser };
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -10,7 +10,7 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: [true, 'Por favor adicione um email'],
     },
-    email: {
+    password: {
       type: String,
       required: [true, 'Por favor adicione uma senha'],
     },

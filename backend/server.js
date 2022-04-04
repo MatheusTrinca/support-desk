4dd5c7368aac91a6a6de371894788c1951bb9c89
Matheus Trinca <matheusfelipetrinca@gmail.com>
2022-04-04 02:29:19 -0300

    connect

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 const express = require('express');
-const app = express();
+const colors = require('colors');
 const PORT = process.env.PORT || 5000;
 const { errorHandler } = require('./middlewares/errorMiddleware');
+const connectDB = require('./config/db');
+
+// DB Connection
+connectDB();
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));

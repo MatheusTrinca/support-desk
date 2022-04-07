7e5892ec8d3fe36bd7be47bedf84fba071a33ae2
Matheus Trinca <matheusfelipetrinca@gmail.com>
2022-04-06 23:09:05 -0300

    login/register

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,20 @@
+import { FaQuestionCircle, FaTicketAlt } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      <section className="heading">
+        <h1>Como podemos te ajudar?</h1>
+        <p>Escolha uma opção abaixo</p>
+      </section>
+      <Link to="/new-ticket" className="btn btn-reverse btn-block">
+        <FaQuestionCircle /> Criar novo chamado
+      </Link>
+      <Link to="/ticket" className="btn btn-block">
+        <FaTicketAlt /> Ver meus chamados
+      </Link>
+    </>
   );
 };
 
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,66 @@
+import { useState } from 'react';
+import { FaUser } from 'react-icons/fa';
+import { toast } from 'react-toastify';
+
 const Login = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  const onChange = e => {
+    setFormData(prevState => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <>
+      <section className="heading">
+        <h1>
+          <FaUser /> Entrar
+        </h1>
+        <p>Entre para usar o Support Desk</p>
+      </section>
+      <section className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              name="email"
+              value={email}
+              onChange={onChange}
+              placeholder="Digite seu email"
+              require
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              name="password"
+              value={password}
+              onChange={onChange}
+              placeholder="Digite sua senha"
+              require
+            />
+          </div>
+          <div className="form-group">
+            <button className="btn btn-block">Entrar</button>
+          </div>
+        </form>
+      </section>
+    </>
   );
 };
 
